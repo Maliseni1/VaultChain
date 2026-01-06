c45deec2a881eacb95d49f4326f389dd375cea45
Hellen Zulu <hellenzulu004@gmail.com>
2026-01-06 03:10:54 +0200

    Added landing page.

--- client/app/page.js
+++ client/app/page.js
@@ -7,21 +7,23 @@ export default function Home() {
       <div className="w-full max-w-6xl bg-white rounded-3xl shadow-sm p-8 md:p-16">
         
         {/* Navbar */}
-        <div className="flex items-center justify-between mb-20">
-          <h1 className="text-xl font-semibold text-gray-800">VaultChain</h1>
+        <nav className="flex items-center justify-between mb-20">
+          <h1 className="text-xl font-semibold text-gray-900">VaultChain</h1>
 
-          <button className="border border-gray-300 px-4 py-2 rounded-lg text-sm text-gray-800 hover:bg-gray-900 hover:text-white hover:border-gray-900 transition-all duration-200">
+          <button 
+          onClick={() => alert("Connect Wallet")}
+          className="border border-gray-300 px-4 py-2 rounded-lg text-sm text-gray-800 hover:bg-gray-900 hover:text-white hover:border-gray-900 transition-all duration-200">
             Connect Wallet
           </button>
-        </div>
+        </nav>
 
         {/* Hero Section */}
         <div className="text-center max-w-2xl mx-auto">
-          <h2 className="text-4xl md:text-5xl font-bold text-gray-500 mb-6">
+          <h2 className="text-4xl md:text-5xl font-bold text-gray-700 mb-6">
             Secure your passwords <br /> on the blockchain
           </h2>
 
-          <p className="text-gray-500 mb-10 text-lg">
+          <p className="text-gray-700 mb-10 text-lg">
             Encrypted credentials stored decentralised. Only you control access.
           </p>
 
